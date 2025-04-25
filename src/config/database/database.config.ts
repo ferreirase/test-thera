98ce54067422232cc37c8a31fb0093b8114b5038
Anderson Raphael Ferreira <phaell.anderson09gmail.com>
2025-04-25 14:43:00 -0300

    feat: update database configuration

--- src/config/database/database.config.ts
+++ src/config/database/database.config.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
-  database: join(process.cwd(), 'database.sqlite'),
+  database: join(process.cwd(), 'src/config/database/db.sqlite'),
   entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
   synchronize: true, // Set to false in production
   logging: true,
